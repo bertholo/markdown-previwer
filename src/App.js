1e45e0c64efc4250dfade181e9cc058bec9aced6
Joao Bertholo <joao.bertholo@gmail.com>
2024-08-05 12:53:20 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
+import { Col, Container, Form, Row, Card } from 'react-bootstrap';
 import './App.css';
+import { useState } from 'react';
+import { marked } from 'marked';
 
 function App() {
+  const [text, setText] = useState('');
+
+  const updateText = (e) => {
+    setText(e.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="bg-dark ">
+      <h1 className="text-center mb-4">Markdown Preview</h1>
+      <Row>
+        <Col md={6} className="mb-4">
+          <Card>
+            <Card.Header className="shadow bg-primary text-center">Input Text</Card.Header>
+            <Card.Body className='bg-primary-subtle'>
+                <Form.Control
+                  as="textarea"
+                  rows={20}
+                  id="editor"
+                  value={text}
+                  onChange={updateText}
+                  placeholder="Enter Markdown here..."
+                  className='bg-primary-subtle'
+                />
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col md={6} className="mb-4">
+          <Card>
+            <Card.Header className="bg-success text-center">Preview</Card.Header>
+            <Card.Body className="bg-success-subtle">
+              <Preview markdown={text} />
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+function Preview({ markdown }) {
+  const getMarkdownText = () => {
+    const rawMarkup = marked.parse(markdown);
+    return { __html: rawMarkup };
+  };
+
+  return (
+    <div
+      dangerouslySetInnerHTML={getMarkdownText()}
+      id="preview"
+    />
   );
 }
 
